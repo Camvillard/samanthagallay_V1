b845fada4776bc81bb0fc50c7f6caed13c363989
camille villard <hello@camillevillard.com>
2019-01-07 18:41:37 -0500

    added breakpoints for devices & high res screens

--- single.php
+++ single.php
@@ -17,21 +17,39 @@
 			<?php endif; ?>
 			<!-- /post thumbnail -->
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+      <!-- post-details -->
+      <div class="post-details">
+  			<div class="date">publié le <?php the_time('j F Y'); ?></div>
+  			<div class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></div>
+      </div>
+      <!-- /post-details -->
 
-			<!-- post details -->
-			<span class="date"><?php the_time('Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
+      <!-- post title -->
+      <h1>
+        <?php the_title(); ?>
+      </h1>
+      <!-- /post title -->
 
-			<?php the_content(); // Dynamic Content ?>
+      <!-- content -->
+      <div class="content">
+			 <?php the_content(); // Dynamic Content ?>
+      </div>
+      <!-- /content -->
 
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+      <!-- post-footer -->
+      <div class="post-footer">
+        <div class="post-tags">
+			   <?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+        </div>
+
+
+
+      </div>
+      <!-- /post-footer -->
+
+
+
+
 
 			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
 
