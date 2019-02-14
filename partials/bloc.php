cb47bd536b829dd11bf05e585247b0013cdeb965
camille villard <hello@camillevillard.com>
2019-02-14 16:15:41 -0500

    added styles for blocks

--- partials/bloc.php
+++ partials/bloc.php
@@ -1,3 +1,9 @@
-<h3>bloc pour les honraires</h3>
+<h5>bloc pour les domaines d'expertise</h5>
 
-<h4></h4>
+<div id="expertise-bloc">
+  <h3>titre</h3>
+  <hr class="border-md">
+  <p>Boggarts lavender robes, Hermione Granger Fantastic Beasts and Where to Find Them. Bee in your bonnet Hand of Glory elder wand, Flavor Beans Impedimenta. Stunning spells tap-dancing spider.</p>
+</div>
+
+
